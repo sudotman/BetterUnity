7da4ea1e37906373b5ec6eefb22f9cbf16bc57d7
Satyam Kashyap <satyamsudo@gmail.com>
2022-07-04 18:31:12 +0530

    Added new stuff plus a demo

---
+++ Assets/BetterUnity/Scripts/Misc/LogToScreen.cs
@@ -0,0 +1,97 @@
+using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class LogToScreen : MonoBehaviour
+{
+    string tempLogString;
+    Queue logQueue = new Queue();
+
+    private GUIStyle defaultGUIStyle;
+
+    void Start()
+    {
+        Debug.Log("Log1");
+        Debug.Log("Log2");
+        Debug.Log("Log3");
+        Debug.Log("Log4");
+
+        Debug.LogError("This is an error plus time: " + Time.time);
+
+        Debug.LogWarning("Warning plus position: " + transform.position);
+
+        Debug.LogAssertion("Assertion plus time: " + Time.deltaTime);
+
+        //a repeating function to test logging
+        InvokeRepeating("LogTest", 1, 2);
+
+        //remove debug elements after a certain time
+        InvokeRepeating("RemoveInTime", 2, 5);
+
+        //Set the default style for our GUI elements
+        defaultGUIStyle = new GUIStyle(EditorStyles.label);
+        defaultGUIStyle.normal.textColor = Color.white; //We can multiply our elements with any color later to get our desired color
+    }
+
+    void OnEnable()
+    {
+        
+
+        Application.logMessageReceived += HandleLog;
+    }
+
+    void OnDisable()
+    {
+        Application.logMessageReceived -= HandleLog;
+    }
+
+    void HandleLog(string logString, string stackTrace, LogType type)
+    {
+        tempLogString = logString;
+        string newString = "\n [" + type + "] " + tempLogString;
+        logQueue.Enqueue(newString);
+        if (type == LogType.Exception)
+        {
+            newString = "\n" + stackTrace;
+            logQueue.Enqueue(newString);
+        }
+        tempLogString = string.Empty;
+        foreach (string mylog in logQueue)
+        {
+            tempLogString += mylog;
+        }
+
+        if (logQueue.Count > 21)
+        {
+            logQueue.Dequeue();
+        }
+    }
+
+    void LogTest()
+    {
+        Debug.Log("Current time: " + Time.time);
+    }
+
+    void OnGUI()
+    {
+        GUI.contentColor = Color.yellow;
+        GUILayout.Label(tempLogString);
+    }
+
+    void RemoveInTime()
+    {
+        //Debug.Log("remove in time");
+        if (logQueue.Count > 0)
+        {
+            logQueue.Dequeue();
+        }
+    }
+
+    //IEnumerator removeInTime(string dequeue)
+    //{
+    //    yield return new WaitForSeconds(4f);
+    //    myLogQueue.Dequeue();
+    //}
+
+
+}
